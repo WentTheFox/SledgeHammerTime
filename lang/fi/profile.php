b874e97267e70a915d28c3736567b7be2e028df3
Double Colon <wentthefox@protonmail.com>
2025-06-19 04:45:04 +0200

    New translations profile.php (Finnish)

--- lang/fi/profile.php
+++ lang/fi/profile.php
@@ -14,11 +14,20 @@ return [
       'A HammerTime account is going to let you link multiple Discord accounts to be able to manage their resources in one place in the future.',
       'Below you can find a list of all Discord accounts associated with this HammerTime account.',
     ],
+    'discord' => 'Discord Accounts',
+    'crowdin' => 'Crowdin Accounts',
     'noConnectedAccounts' => 'You do not have any accounts connected at the moment',
     'linkAdditional' => [
       'heading' => 'Link or Update Account',
-      'description' => 'By linking multiple Discord accounts to a single HammerTime account, you can manage all connected accounts\' settings in a single place. If your profile picture or display name changed, you can also use this to update that information for any account which is already linked.',
-      'authorize' => 'Authorize Discord account',
+      'description' => 'By linking multiple accounts to a single HammerTime account, you can manage all connected accounts\' settings in a single place.',
+      'discord' => [
+        'description' => 'If your profile picture or display name changed, you can also use this to update that information for any Discord account which is already linked.',
+        'authorize' => 'Authorize Discord account',
+      ],
+      'crowdin' => [
+        'description' => 'If you are a translator for the project, linking your Crowdin account will eventually let you customize how your name appears in the translation credits, and also serves as an extra level of verification that these accounts belong to you in case you join the project\'s Discord server.',
+        'authorize' => 'Authorize Crowdin account',
+      ],
     ],
   ],
   'deletion' => [
