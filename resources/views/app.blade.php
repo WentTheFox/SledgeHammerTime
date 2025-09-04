6c81596d7e8dd185b45dafd548f8e312161d778e
WentTheFox <mail@went.tf>
2025-09-04 02:36:05 +0200

    Fix <head> opening tag in index blade template

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" dir="ltr">
-<me>
+<head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
@@ -33,8 +33,8 @@
   @routes
   @vite(['resources/js/app.ts', "resources/js/Pages/{$page['component']}.vue"])
   @inertiaHead
-  </head>
-  <body class="no-anim">
-  @inertia
-  </body>
+</head>
+<body class="no-anim">
+@inertia
+</body>
 </html>
