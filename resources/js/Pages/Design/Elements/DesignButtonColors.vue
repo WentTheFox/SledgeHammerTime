7ddcb66d4389e0b0fa74f499ea6e6fa959484475
WentTheFox <mail@went.tf>
2025-09-05 22:12:42 +0200

    Add all button variations to design page

---
+++ resources/js/Pages/Design/Elements/DesignButtonColors.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import HtButton from '@/Reusable/HtButton.vue';
+import { ButtonColors } from '@/utils/buttons';
+import { faClipboard } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+defineProps<{
+  color?: ButtonColors;
+}>();
+</script>
+
+<template>
+  <div class="mt-2">
+    <HtButton :color="color">
+      Default {{ color }} button
+    </HtButton>
+    &nbsp;
+    <HtButton
+      :color="color"
+      :disabled="true"
+    >
+      Disabled {{ color }} button
+    </HtButton>
+    &nbsp;
+    <HtButton
+      :color="color"
+      :loading="true"
+    >
+      Loading {{ color }} button
+    </HtButton>
+    &nbsp;
+    <HtButton :color="color">
+      <FontAwesomeIcon :icon="faClipboard" />
+    </HtButton>
+    &nbsp;
+    <HtButton
+      :color="color"
+      :disabled="true"
+    >
+      <FontAwesomeIcon :icon="faClipboard" />
+    </HtButton>
+    &nbsp;
+    <HtButton
+      :color="color"
+      :loading="true"
+    >
+      <FontAwesomeIcon :icon="faClipboard" />
+    </HtButton>
+  </div>
+</template>
--- resources/js/Pages/Design/Elements/DesignButtons.vue
+++ resources/js/Pages/Design/Elements/DesignButtons.vue
@@ -1,72 +1,25 @@
 <script setup lang="ts">
-import HtButton from '@/Reusable/HtButton.vue';
-import { faClipboard } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import DesignButtonColors from '@/Pages/Design/Elements/DesignButtonColors.vue';
+import { ButtonColors } from '@/utils/buttons';
+
+const buttonColors: Record<ButtonColors, true> = {
+  'primary': true,
+  'success': true,
+  'danger': true,
+  'warning': true,
+  'light': true,
+  'dark': true,
+};
 </script>
 
 <template>
   <p class="mb-3">
     Buttons
   </p>
-  <div>
-    <HtButton>Simple button</HtButton>
-    &nbsp;
-    <HtButton :disabled="true">
-      Disabled default button
-    </HtButton>
-    &nbsp;
-    <HtButton :loading="true">
-      Loading default button
-    </HtButton>
-    &nbsp;
-    <HtButton>
-      <FontAwesomeIcon :icon="faClipboard" />
-    </HtButton>
-    &nbsp;
-    <HtButton :disabled="true">
-      <FontAwesomeIcon :icon="faClipboard" />
-    </HtButton>
-    &nbsp;
-    <HtButton :loading="true">
-      <FontAwesomeIcon :icon="faClipboard" />
-    </HtButton>
-  </div>
-  <div>&nbsp;</div>
-  <div>
-    <HtButton color="primary">
-      Primary button
-    </HtButton>
-    &nbsp;
-    <HtButton
-      color="primary"
-      :disabled="true"
-    >
-      Disabled primary button
-    </HtButton>
-    &nbsp;
-    <HtButton
-      color="primary"
-      :loading="true"
-    >
-      Loading primary button
-    </HtButton>
-    &nbsp;
-    <HtButton color="primary">
-      <FontAwesomeIcon :icon="faClipboard" />
-    </HtButton>
-    &nbsp;
-    <HtButton
-      color="primary"
-      :disabled="true"
-    >
-      <FontAwesomeIcon :icon="faClipboard" />
-    </HtButton>
-    &nbsp;
-    <HtButton
-      color="primary"
-      :loading="true"
-    >
-      <FontAwesomeIcon :icon="faClipboard" />
-    </HtButton>
-  </div>
+  <DesignButtonColors />
+  <DesignButtonColors
+    v-for="(_, key) in buttonColors"
+    :key="key"
+    :color="key"
+  />
 </template>
