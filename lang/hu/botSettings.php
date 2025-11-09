ff192e8e1f64bf9a44d3da285d5eb8c3272f5034
Double Colon <wentthefox@protonmail.com>
2025-11-09 05:50:02 +0100

    New translations botsettings.php (Hungarian)

--- lang/hu/botSettings.php
+++ lang/hu/botSettings.php
@@ -6,12 +6,8 @@ return [
   'learnMore' => 'Nem tudod, mi ez az app, vagy kíváncsi vagy mit tud? Látogasd meg a <1>Discord App</1> linket további információért.',
   'advancedSettings' => [
     'toggleText' => 'Haladó beállítások',
-    'atCommandName' => 'ekkor',
-    'at12CommandName' => 'ekkor12',
-    'hourOptionName' => 'óra',
-    'minuteOptionName' => 'perc',
-    'secondOptionName' => 'másodperc',
   ],
+  'defaultOption' => 'Default',
   'fields' => [
     'rawData' => [
       'displayName' => 'Nyers adatok (hibakeresés céljából)',
@@ -21,28 +17,12 @@ return [
     ],
     'format' => [
       'displayName' => 'Formátum',
-      'option' => [
-        'd' => 'Rövid dátum',
-        'f' => 'Dátum és idő',
-        't' => 'Rövid idő',
-        'D' => 'Hosszú dátum',
-        'F' => 'Hét napja, dátum és idő',
-        'R' => 'Viszonylagos',
-        'T' => 'Hosszú idő',
-        'default' => 'Alapértelmezett',
-      ],
     ],
     'formatMinimalReply' => [
       'displayName' => 'Csak előnézet a válaszban a formátum opció használatakor',
     ],
     'columns' => [
       'displayName' => 'Oszlopok',
-      'option' => [
-        'syntax' => 'Csak a kód',
-        'preview' => 'Csak az előnézet',
-        'both' => 'A kód és Az előnézet is',
-        'default' => 'Alapértelmezett',
-      ],
     ],
     'ephemeral' => [
       'displayName' => 'Ideiglenes',
