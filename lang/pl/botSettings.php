e860f0c6bc2223af3068057d1580b2fcb3f9fb37
Double Colon <wentthefox@protonmail.com>
2025-11-12 16:01:34 +0100

    New translations botsettings.php (Polish)

--- lang/pl/botSettings.php
+++ lang/pl/botSettings.php
@@ -10,7 +10,7 @@ return [
   'defaultOption' => 'Default',
   'fields' => [
     'timezone' => [
-      'displayName' => 'Timezone',
+      'displayName' => 'Strefa czasowa',
     ],
     'format' => [
       'displayName' => 'Format',
