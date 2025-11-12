26df2e7bb3f64023938daa3228f8e466db7faa4f
Double Colon <wentthefox@protonmail.com>
2025-11-12 16:06:41 +0100

    New translations timestamppicker.php (Esperanto)

---
+++ lang/eo/timestampPicker.php
@@ -0,0 +1,54 @@
+<?php
+
+return [
+  'howTo' => 'Pick a date, copy the desired timestamp from the :syntaxColName column, then paste it anywhere in a chat message. The result will be a dynamic timestamp that displays differently for everyone based on their own timezone.',
+  'picker' => [
+    'label' => [
+      'date' => 'Dato',
+      'time' => 'Horo',
+      'dateAndTime' => 'Dato kaj horo',
+      'timezone' => 'Horzono',
+      'modeOffset' => 'Absolute Offset',
+      'modeZoneName' => 'Zone Name',
+    ],
+    'button' => [
+      'jumpToToday' => 'Jump to current month',
+    ],
+    'tooltip' => [
+      'setToCurrent' => 'Agordi al nuna tempo',
+      'lock' => 'Ŝlosi tempindikon per URL',
+      'unlock' => 'Malŝlosi tempindikon',
+      'previousYear' => 'Previous year',
+      'previousMonth' => 'Previous month',
+      'nextMonth' => 'Next month',
+      'nextYear' => 'Next year',
+    ],
+  ],
+  'table' => [
+    'syntaxColumn' => 'Babilsintakso',
+    'resultColumn' => 'Ekzempla rezulto',
+  ],
+  'usefulLinks' => [
+    'lead' => 'You may also find these useful:',
+    'server' => [
+      'header' => 'Official HammerTime Server',
+      'p' => 'Discuss the website, test the syntax and suggest features',
+    ],
+    'bot' => [
+      'header' => 'HammerTime App',
+      'p' => 'Generate timestamps from within Discord using slash commands',
+    ],
+    'oldSite' => [
+      'header' => 'HammerTime Old Website',
+      'p' => 'The old website of the project, still available until this Beta goes live',
+    ],
+    'textColor' => [
+      'header' => 'Rebane\'s <1>Colored</1> Text Generator',
+      'p' => 'A simple app that creates colored Discord messages using ANSI color codes',
+    ],
+    "subreddit" => [
+      "header" => "r/SplitSecond",
+      "p" => "The community hosting weekly challenges for an underrated racing game that inspired the creation of this project",
+    ],
+  ],
+];
