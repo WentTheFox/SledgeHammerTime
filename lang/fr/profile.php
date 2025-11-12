eb93c2158829671dbfb6ff175834fd73e5158411
Double Colon <wentthefox@protonmail.com>
2025-11-12 14:42:55 +0100

    New translations profile.php (French)

--- lang/fr/profile.php
+++ lang/fr/profile.php
@@ -1,29 +1,29 @@
 <?php
 
 return [
-  'title' => 'Profile',
+  'title' => 'Profil',
   'intro' => 'The profile page allows you to manage your account information specifically related to the website. If you are looking for where to change defaults and other settings for the Discord app, visit the <1>App Settings</1> page.',
   'information' => [
-    'heading' => 'Profile Information',
-    'description' => "Update your account's profile information.",
-    'displayName' => 'Display Name',
-    'saveSuccess' => 'Your profile has been updated successfully.',
+    'heading' => 'Information du profil',
+    'description' => "Mettre à jour les informations de profil de votre compte.",
+    'displayName' => 'Nom d\'affichage',
+    'saveSuccess' => 'Votre profil a été mis à jour avec succès.',
   ],
   'accounts' => [
-    'heading' => 'Connected Accounts',
+    'heading' => 'Comptes connectés',
     'description' => [
-      'A HammerTime account is going to let you link multiple Discord accounts to be able to manage their resources in one place in the future.',
-      'Below you can find a list of all Discord accounts associated with this HammerTime account.',
+      'Un compte HammerTime vous permettra de relier plusieurs comptes Discord afin de pouvoir gérer leurs ressources en un seul endroit à l\'avenir.',
+      'Vous trouverez ci-dessous une liste de tous les comptes Discord associés à ce compte HammerTime.',
     ],
     'discord' => 'Discord Accounts',
     'crowdin' => 'Crowdin Accounts',
-    'noConnectedAccounts' => 'You do not have any accounts connected at the moment',
+    'noConnectedAccounts' => 'Vous n\'avez pas de comptes connectés pour le moment',
     'linkAdditional' => [
-      'heading' => 'Link or Update Account',
+      'heading' => 'Lier ou mettre à jour un compte',
       'description' => 'By linking multiple accounts to a single HammerTime account, you can manage all connected accounts\' settings in a single place.',
       'discord' => [
         'description' => 'If your profile picture or display name changed, you can also use this to update that information for any Discord account which is already linked.',
-        'authorize' => 'Authorize Discord account',
+        'authorize' => 'Autoriser le compte Discord',
       ],
       'crowdin' => [
         'description' => 'If you are a translator for the project, linking your Crowdin account will eventually let you customize how your name appears in the translation credits, and also serves as an extra level of verification that these accounts belong to you in case you join the project\'s Discord server.',
@@ -32,12 +32,12 @@ return [
     ],
   ],
   'deletion' => [
-    'heading' => 'Account Deletion',
-    'description' => 'Once your account is deleted, all of its resources and data will be permanently deleted. Before deleting your account, please download any data or information that you wish to retain.',
-    'deleteButton' => 'Delete Account',
+    'heading' => 'Suppression de compte',
+    'description' => 'Une fois votre compte supprimé, toutes ses ressources et données seront définitivement effacées. Avant de supprimer votre compte, veuillez télécharger toutes les données ou informations que vous souhaitez conserver.',
+    'deleteButton' => 'Supprimer le compte',
     'confirmDialog' => [
-      'header' => 'Are you sure you want to delete your account?',
-      'body' => 'Once your account is deleted, all of its resources and data will be permanently deleted.',
+      'header' => 'Êtes-vous sûr de vouloir supprimer votre compte ?',
+      'body' => 'Une fois votre compte supprimé, toutes ses ressources et données seront définitivement effacées.',
     ],
   ],
 ];
