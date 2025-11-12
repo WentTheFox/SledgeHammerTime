4a2a80d627ef78650dd464702b3840bb44f3a615
Double Colon <wentthefox@protonmail.com>
2025-11-12 14:41:44 +0100

    New translations botsettings.php (Norwegian Bokmal)

--- lang/nb/botSettings.php
+++ lang/nb/botSettings.php
@@ -10,7 +10,7 @@ return [
   'defaultOption' => 'Default',
   'fields' => [
     'timezone' => [
-      'displayName' => 'Timezone',
+      'displayName' => 'Tidssone',
     ],
     'format' => [
       'displayName' => 'Format',
