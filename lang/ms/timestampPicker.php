5a56d293e7996e6b4b16b233ca294f790669856a
Double Colon <wentthefox@protonmail.com>
2025-11-12 16:04:50 +0100

    New translations timestamppicker.php (Malay)

--- lang/ms/timestampPicker.php
+++ lang/ms/timestampPicker.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-  'howTo' => 'Pick a date, copy the desired timestamp from the :syntaxColName column, then paste it anywhere in a chat message. The result will be a dynamic timestamp that displays differently for everyone based on their own timezone.',
+  'howTo' => 'Pilih tarikh, salin cap masa yang diingini dari lajur :syntaxColName, kemudian tampal di mana-mana sahaja dalam mesej sembang. Hasilnya ialah cap masa dinamik yang dipaparkan secara berlainan untuk semua orang berdasarkan zon waktu masing-masing.',
   'picker' => [
     'label' => [
       'date' => 'Tarikh',
