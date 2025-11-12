570d92739ab35be8428292398bc6719f75e303a8
Double Colon <wentthefox@protonmail.com>
2025-11-12 14:43:28 +0100

    New translations timestamppicker.php (Czech)

--- lang/cs/timestampPicker.php
+++ lang/cs/timestampPicker.php
@@ -4,10 +4,10 @@ return [
   'howTo' => 'Pick a date, copy the desired timestamp from the :syntaxColName column, then paste it anywhere in a chat message. The result will be a dynamic timestamp that displays differently for everyone based on their own timezone.',
   'picker' => [
     'label' => [
-      'date' => 'Date',
-      'time' => 'Time',
-      'dateAndTime' => 'Date and time',
-      'timezone' => 'Timezone',
+      'date' => 'Datum',
+      'time' => 'Čas',
+      'dateAndTime' => 'Datum a čas',
+      'timezone' => 'Časové pásmo',
       'modeOffset' => 'Absolute Offset',
       'modeZoneName' => 'Zone Name',
     ],
@@ -15,9 +15,9 @@ return [
       'jumpToToday' => 'Jump to current month',
     ],
     'tooltip' => [
-      'setToCurrent' => 'Set to current time',
-      'lock' => 'Lock timestamp via URL',
-      'unlock' => 'Unlock timestamp',
+      'setToCurrent' => 'Nastavit na aktuální čas',
+      'lock' => 'Uzamknout časové razítko pomocí URL',
+      'unlock' => 'Odemknout časové razítko',
       'previousYear' => 'Previous year',
       'previousMonth' => 'Previous month',
       'nextMonth' => 'Next month',
@@ -25,30 +25,30 @@ return [
     ],
   ],
   'table' => [
-    'syntaxColumn' => 'Chat syntax',
-    'resultColumn' => 'Example result',
+    'syntaxColumn' => 'Syntax chatu',
+    'resultColumn' => 'Příklad výsledku',
   ],
   'usefulLinks' => [
-    'lead' => 'You may also find these useful:',
+    'lead' => 'Užitečné pro vás mohou být také tyto:',
     'server' => [
-      'header' => 'Official HammerTime Server',
-      'p' => 'Discuss the website, test the syntax and suggest features',
+      'header' => 'Oficální HammerTime Server',
+      'p' => 'Diskutujte o webových stránkách, testujte syntaxi a navrhujte funkce',
     ],
     'bot' => [
       'header' => 'HammerTime App',
-      'p' => 'Generate timestamps from within Discord using slash commands',
+      'p' => 'Generování časových razítek přímo z aplikace Discord pomocí lomítkových příkazů',
     ],
     'oldSite' => [
       'header' => 'HammerTime Old Website',
       'p' => 'The old website of the project, still available until this Beta goes live',
     ],
     'textColor' => [
-      'header' => 'Rebane\'s <1>Colored</1> Text Generator',
-      'p' => 'A simple app that creates colored Discord messages using ANSI color codes',
+      'header' => 'Generátor <1>barevného</1> textu od společnosti Rebane',
+      'p' => 'Jednoduchá aplikace, která vytváří barevné zprávy Discord pomocí barevných kódů ANSI',
     ],
     "subreddit" => [
       "header" => "r/SplitSecond",
-      "p" => "The community hosting weekly challenges for an underrated racing game that inspired the creation of this project",
+      "p" => "Komunita pořádající týdenní výzvy pro podceňovanou závodní hru, která inspirovala vznik tohoto projektu",
     ],
   ],
 ];
