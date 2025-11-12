3f86fcb24db4f3947b5eee206b9d8892dcfef815
Double Colon <wentthefox@protonmail.com>
2025-11-12 14:43:56 +0100

    New translations timestamppicker.php (Chinese Simplified)

--- lang/zh_CN/timestampPicker.php
+++ lang/zh_CN/timestampPicker.php
@@ -4,10 +4,10 @@ return [
   'howTo' => 'Pick a date, copy the desired timestamp from the :syntaxColName column, then paste it anywhere in a chat message. The result will be a dynamic timestamp that displays differently for everyone based on their own timezone.',
   'picker' => [
     'label' => [
-      'date' => 'Date',
-      'time' => 'Time',
-      'dateAndTime' => 'Date and time',
-      'timezone' => 'Timezone',
+      'date' => '日期',
+      'time' => '时间',
+      'dateAndTime' => '日期与时间',
+      'timezone' => '时区',
       'modeOffset' => 'Absolute Offset',
       'modeZoneName' => 'Zone Name',
     ],
@@ -15,9 +15,9 @@ return [
       'jumpToToday' => 'Jump to current month',
     ],
     'tooltip' => [
-      'setToCurrent' => 'Set to current time',
-      'lock' => 'Lock timestamp via URL',
-      'unlock' => 'Unlock timestamp',
+      'setToCurrent' => '设为当前时间',
+      'lock' => '通过 URL 锁定时间戳',
+      'unlock' => '解锁时间戳',
       'previousYear' => 'Previous year',
       'previousMonth' => 'Previous month',
       'nextMonth' => 'Next month',
@@ -25,30 +25,30 @@ return [
     ],
   ],
   'table' => [
-    'syntaxColumn' => 'Chat syntax',
-    'resultColumn' => 'Example result',
+    'syntaxColumn' => '代码语法',
+    'resultColumn' => '示例显示效果',
   ],
   'usefulLinks' => [
-    'lead' => 'You may also find these useful:',
+    'lead' => '也许您也想看看这些链接：',
     'server' => [
-      'header' => 'Official HammerTime Server',
-      'p' => 'Discuss the website, test the syntax and suggest features',
+      'header' => '官方 HammerTime 服务器',
+      'p' => '讨论本网站，测试语法，提出建议',
     ],
     'bot' => [
       'header' => 'HammerTime App',
-      'p' => 'Generate timestamps from within Discord using slash commands',
+      'p' => '使用斜杠指令在 Discord 内生成时间戳',
     ],
     'oldSite' => [
       'header' => 'HammerTime Old Website',
       'p' => 'The old website of the project, still available until this Beta goes live',
     ],
     'textColor' => [
-      'header' => 'Rebane\'s <1>Colored</1> Text Generator',
-      'p' => 'A simple app that creates colored Discord messages using ANSI color codes',
+      'header' => 'Rebane 的<1>彩色</1>文本生成器',
+      'p' => '一个简单的应用，使用 ANSI 颜色代码创建带颜色文本的 Discord 消息',
     ],
     "subreddit" => [
       "header" => "r/SplitSecond",
-      "p" => "The community hosting weekly challenges for an underrated racing game that inspired the creation of this project",
+      "p" => "这个Reddit社群讨论一款好玩不火的赛车游戏，其举办的每周挑战赛是本项目的创作灵感",
     ],
   ],
 ];
