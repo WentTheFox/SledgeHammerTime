d16b94dbf8d034cc2defa78101aad9a5a3356d07
Double Colon <wentthefox@protonmail.com>
2025-11-12 16:06:44 +0100

    New translations addapp.php (Esperanto)

---
+++ lang/eo/addApp.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+  "title" => "Elektu kie vi volas aldoni la aplikaĵon",
+  "user" => [
+    "title" => "Aldonu en Miajn Aplikaĵojn",
+    "description" => "Uzu ĉi tiun aplikaĵon ĉie!",
+  ],
+  "guild" => [
+    "title" => "Aldonu en servilon",
+    "description" => "Personigu vian servilon per aldoni ĉi tiun aplikaĵon",
+  ],
+];
