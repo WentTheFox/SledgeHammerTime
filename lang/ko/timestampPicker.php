3cd620c343c8fd1817b4793366399d35f5555e4e
Double Colon <wentthefox@protonmail.com>
2025-11-12 14:43:49 +0100

    New translations timestamppicker.php (Korean)

--- lang/ko/timestampPicker.php
+++ lang/ko/timestampPicker.php
@@ -4,10 +4,10 @@ return [
   'howTo' => 'Pick a date, copy the desired timestamp from the :syntaxColName column, then paste it anywhere in a chat message. The result will be a dynamic timestamp that displays differently for everyone based on their own timezone.',
   'picker' => [
     'label' => [
-      'date' => 'Date',
-      'time' => 'Time',
-      'dateAndTime' => 'Date and time',
-      'timezone' => 'Timezone',
+      'date' => '날짜',
+      'time' => '시간',
+      'dateAndTime' => '일시',
+      'timezone' => '시간대',
       'modeOffset' => 'Absolute Offset',
       'modeZoneName' => 'Zone Name',
     ],
@@ -15,9 +15,9 @@ return [
       'jumpToToday' => 'Jump to current month',
     ],
     'tooltip' => [
-      'setToCurrent' => 'Set to current time',
-      'lock' => 'Lock timestamp via URL',
-      'unlock' => 'Unlock timestamp',
+      'setToCurrent' => '현재 시간으로 설정',
+      'lock' => 'URL로 타임스탬프 잠그기',
+      'unlock' => '타임스탬프 잠금 풀기',
       'previousYear' => 'Previous year',
       'previousMonth' => 'Previous month',
       'nextMonth' => 'Next month',
@@ -25,30 +25,30 @@ return [
     ],
   ],
   'table' => [
-    'syntaxColumn' => 'Chat syntax',
-    'resultColumn' => 'Example result',
+    'syntaxColumn' => '메시지 형식',
+    'resultColumn' => '결과 예시',
   ],
   'usefulLinks' => [
-    'lead' => 'You may also find these useful:',
+    'lead' => '유용할 수도 있는 링크들',
     'server' => [
-      'header' => 'Official HammerTime Server',
-      'p' => 'Discuss the website, test the syntax and suggest features',
+      'header' => '공식 HammerTime 서버',
+      'p' => '웹사이트에 대한 토론, 타임스탬프의 테스트, 기능의 요청을 하실 수 있습니다.',
     ],
     'bot' => [
-      'header' => 'HammerTime App',
-      'p' => 'Generate timestamps from within Discord using slash commands',
+      'header' => 'HammerTime 앱',
+      'p' => '슬래시 명령어를 사용하여 Discord 내에서 타임스탬프를 생성하세요.',
     ],
     'oldSite' => [
       'header' => 'HammerTime Old Website',
       'p' => 'The old website of the project, still available until this Beta goes live',
     ],
     'textColor' => [
-      'header' => 'Rebane\'s <1>Colored</1> Text Generator',
-      'p' => 'A simple app that creates colored Discord messages using ANSI color codes',
+      'header' => 'Rebane님의 <1>컬러</1> 텍스트 생성기',
+      'p' => 'ANSI 컬러 코드를 사용해서 Discord용 컬러 텍스트를 생성하는 간단한 앱',
     ],
     "subreddit" => [
       "header" => "r/SplitSecond",
-      "p" => "The community hosting weekly challenges for an underrated racing game that inspired the creation of this project",
+      "p" => "이 프로젝트 제작에 영감을 준 저평가된 레이싱 게임들을 위한 주간 챌린지를 개최하는 커뮤니티",
     ],
   ],
 ];
