a3e585f0365d4cd8073769383c91b08ef3754146
Double Colon <wentthefox@protonmail.com>
2025-11-17 01:31:22 +0100

    New translations global.php (Portuguese)

--- lang/pt/global.php
+++ lang/pt/global.php
@@ -23,9 +23,13 @@ return [
   'sidebar' => [
     'inputSettings' => [
       'title' => 'Configurações de Entrada',
-      'customInputs' => [
-        'label' => 'Entradas personalizadas',
-        'description' => "Replaces the browser's default inputs with custom ones that respect your chosen website language",
+      'customDateInput' => [
+        'label' => 'Custom date input',
+        'description' => "Use the date input of the project instead of the browser default",
+      ],
+      'customTimeInput' => [
+        'label' => 'Custom time input',
+        'description' => "Use the time input of the project instead of the browser default",
       ],
       'separateInputs' => [
         'label' => 'Entradas separadas',
