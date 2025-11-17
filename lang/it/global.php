98801d2efe463c92b3656c612305d60e02ffbb4f
Double Colon <wentthefox@protonmail.com>
2025-11-17 20:13:37 +0100

    New translations global.php (Italian)

--- lang/it/global.php
+++ lang/it/global.php
@@ -7,6 +7,15 @@ return [
     "heading" => "Pagina Non Trovata",
     "content" => "Niente da vedere qui.",
   ],
+  "maintenanceMode" => [
+    "heading" => "Maintenance Mode",
+    "content" => [
+      'pleaseWait' => "The application is currently being updated, please try again in a few seconds.",
+      'joinSupportServer' => "If the issue persists for more than a few minutes, please join the Discord support server and let us know.",
+      'contactDeveloper' => "If the issue persists for more than a few minutes, please contact the developer and let them know.",
+    ],
+    "supportServerButton" => "Support Server",
+  ],
   'incompleteTranslations' => 'Le traduzioni sono incomplete',
   'contributeTranslations' => 'Contribuisci',
   'copyToClipboard' => 'Copia negli appunti',
