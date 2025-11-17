7f9f463c855e233b5c2a601368f85f79278f8134
Double Colon <wentthefox@protonmail.com>
2025-11-17 20:13:35 +0100

    New translations global.php (Hebrew)

--- lang/he/global.php
+++ lang/he/global.php
@@ -7,6 +7,15 @@ return [
     "heading" => "Page Not Found",
     "content" => "אין שום דברים לראות פה.",
   ],
+  "maintenanceMode" => [
+    "heading" => "Maintenance Mode",
+    "content" => [
+      'pleaseWait' => "The application is currently being updated, please try again in a few seconds.",
+      'joinSupportServer' => "If the issue persists for more than a few minutes, please join the Discord support server and let us know.",
+      'contactDeveloper' => "If the issue persists for more than a few minutes, please contact the developer and let them know.",
+    ],
+    "supportServerButton" => "Support Server",
+  ],
   'incompleteTranslations' => 'התרגומים לא מלאים',
   'contributeTranslations' => 'עזור בתרגום',
   'copyToClipboard' => 'העתיקו ללוח',
