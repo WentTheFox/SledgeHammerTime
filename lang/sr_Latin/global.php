e63b656ad7a5ffcf907f9460957e19e80f801c7d
Double Colon <wentthefox@protonmail.com>
2025-11-17 01:31:44 +0100

    New translations global.php (Serbian (Latin))

--- lang/sr_Latin/global.php
+++ lang/sr_Latin/global.php
@@ -23,9 +23,13 @@ return [
   'sidebar' => [
     'inputSettings' => [
       'title' => 'Podešavanja unosa',
-      'customInputs' => [
-        'label' => 'Prilagođeni ulazi',
-        'description' => "Replaces the browser's default inputs with custom ones that respect your chosen website language",
+      'customDateInput' => [
+        'label' => 'Custom date input',
+        'description' => "Use the date input of the project instead of the browser default",
+      ],
+      'customTimeInput' => [
+        'label' => 'Custom time input',
+        'description' => "Use the time input of the project instead of the browser default",
       ],
       'separateInputs' => [
         'label' => 'Odvojeni ulazi',
