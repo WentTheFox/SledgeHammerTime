0892898d92d79e5908db9d495c540093cda28d6a
Double Colon <wentthefox@protonmail.com>
2025-11-17 20:13:43 +0100

    New translations global.php (Dutch)

--- lang/nl/global.php
+++ lang/nl/global.php
@@ -7,6 +7,15 @@ return [
     "heading" => "Pagina Niet Gevonden",
     "content" => "Niets te zien hier.",
   ],
+  "maintenanceMode" => [
+    "heading" => "Maintenance Mode",
+    "content" => [
+      'pleaseWait' => "The application is currently being updated, please try again in a few seconds.",
+      'joinSupportServer' => "If the issue persists for more than a few minutes, please join the Discord support server and let us know.",
+      'contactDeveloper' => "If the issue persists for more than a few minutes, please contact the developer and let them know.",
+    ],
+    "supportServerButton" => "Support Server",
+  ],
   'incompleteTranslations' => 'Vertalingen zijn onvolledig',
   'contributeTranslations' => 'Draag bij',
   'copyToClipboard' => 'Kopiëren naar klembord',
