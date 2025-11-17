43c6a7e5597cd28623592baacbc8422c4298fa38
Double Colon <wentthefox@protonmail.com>
2025-11-17 20:13:44 +0100

    New translations global.php (Polish)

--- lang/pl/global.php
+++ lang/pl/global.php
@@ -7,6 +7,15 @@ return [
     "heading" => "Nie znaleziono strony",
     "content" => "Niczego tutaj nie ma.",
   ],
+  "maintenanceMode" => [
+    "heading" => "Maintenance Mode",
+    "content" => [
+      'pleaseWait' => "The application is currently being updated, please try again in a few seconds.",
+      'joinSupportServer' => "If the issue persists for more than a few minutes, please join the Discord support server and let us know.",
+      'contactDeveloper' => "If the issue persists for more than a few minutes, please contact the developer and let them know.",
+    ],
+    "supportServerButton" => "Support Server",
+  ],
   'incompleteTranslations' => 'Tłumaczenia są niekompletne',
   'contributeTranslations' => 'Wspomóż',
   'copyToClipboard' => 'Skopiuj do schowka',
