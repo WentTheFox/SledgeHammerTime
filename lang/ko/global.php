09c76269bd04ab7cce9e0ca1787980a12c3e7417
Double Colon <wentthefox@protonmail.com>
2025-11-17 01:31:16 +0100

    New translations global.php (Korean)

--- lang/ko/global.php
+++ lang/ko/global.php
@@ -23,9 +23,13 @@ return [
   'sidebar' => [
     'inputSettings' => [
       'title' => '입력 설정',
-      'customInputs' => [
-        'label' => '커스텀 입력',
-        'description' => "Replaces the browser's default inputs with custom ones that respect your chosen website language",
+      'customDateInput' => [
+        'label' => 'Custom date input',
+        'description' => "Use the date input of the project instead of the browser default",
+      ],
+      'customTimeInput' => [
+        'label' => 'Custom time input',
+        'description' => "Use the time input of the project instead of the browser default",
       ],
       'separateInputs' => [
         'label' => '별개 입력',
