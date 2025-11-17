e2e1cf3e4e82f4172dc6bbae7a571da706450409
Double Colon <wentthefox@protonmail.com>
2025-11-17 02:20:44 +0100

    New translations global.php (Polish)

--- lang/pl/global.php
+++ lang/pl/global.php
@@ -35,6 +35,10 @@ return [
         'label' => 'Osobne pola',
         'description' => "Wyświetla dwa różne pola na datę i czas zamiast połączonych pól (które są nieobsługiwane przez niektóre przeglądarki)",
       ],
+      'flatUi' => [
+        'label' => 'Flatten interface',
+        'description' => "Disable shadow and height effects on inputs and buttons",
+      ],
     ],
     'timeSync' => [
       'title' => 'Time Synchronization',
