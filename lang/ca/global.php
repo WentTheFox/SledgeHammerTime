34dbcc019df7c730034c0b5c34474c70888e0f78
Double Colon <wentthefox@protonmail.com>
2025-11-17 20:13:29 +0100

    New translations global.php (Catalan)

--- lang/ca/global.php
+++ lang/ca/global.php
@@ -7,6 +7,15 @@ return [
     "heading" => "Page Not Found",
     "content" => "No hi ha res a veure ací.",
   ],
+  "maintenanceMode" => [
+    "heading" => "Maintenance Mode",
+    "content" => [
+      'pleaseWait' => "The application is currently being updated, please try again in a few seconds.",
+      'joinSupportServer' => "If the issue persists for more than a few minutes, please join the Discord support server and let us know.",
+      'contactDeveloper' => "If the issue persists for more than a few minutes, please contact the developer and let them know.",
+    ],
+    "supportServerButton" => "Support Server",
+  ],
   'incompleteTranslations' => 'Traducció incompleta',
   'contributeTranslations' => 'Contribuïu',
   'copyToClipboard' => 'Copia al porta-retalls',
