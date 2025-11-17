df0c7b1b46c21a5d47cae499c748d90bf9e933ee
Double Colon <wentthefox@protonmail.com>
2025-11-17 01:31:16 +0100

    New translations global.php (Japanese)

--- lang/ja/global.php
+++ lang/ja/global.php
@@ -23,9 +23,13 @@ return [
   'sidebar' => [
     'inputSettings' => [
       'title' => '入力設定',
-      'customInputs' => [
-        'label' => 'カスタム入力',
-        'description' => "Replaces the browser's default inputs with custom ones that respect your chosen website language",
+      'customDateInput' => [
+        'label' => 'Custom date input',
+        'description' => "Use the date input of the project instead of the browser default",
+      ],
+      'customTimeInput' => [
+        'label' => 'Custom time input',
+        'description' => "Use the time input of the project instead of the browser default",
       ],
       'separateInputs' => [
         'label' => '分離入力',
