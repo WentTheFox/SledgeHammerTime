e2924c69e4ae52a2e004b18bd2797896dee6a4de
Double Colon <wentthefox@protonmail.com>
2025-11-19 02:16:48 +0100

    New translations passwords.php (Portuguese)

--- lang/pt/passwords.php
+++ lang/pt/passwords.php
@@ -3,9 +3,9 @@
 declare(strict_types=1);
 
 return [
-    'reset'     => 'A palavra-passe foi redefinida!',
-    'sent'      => 'O lembrete para a palavra-passe foi enviado!',
-    'throttled' => 'Por favor aguarde, antes de tentar novamente.',
-    'token'     => 'Este código de recuperação da palavra-passe é inválido.',
-    'user'      => 'Não existe nenhum utilizador com o e-mail indicado.',
+    'reset'     => 'Your password has been reset.',
+    'sent'      => 'We have emailed your password reset link.',
+    'throttled' => 'Please wait before retrying.',
+    'token'     => 'This password reset token is invalid.',
+    'user'      => 'We can\'t find a user with that email address.',
 ];
