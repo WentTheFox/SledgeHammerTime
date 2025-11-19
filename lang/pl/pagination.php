804c350a3e718d1489b6f8a9780acc0906460a70
Double Colon <wentthefox@protonmail.com>
2025-11-19 02:16:10 +0100

    New translations pagination.php (Polish)

--- lang/pl/pagination.php
+++ lang/pl/pagination.php
@@ -3,6 +3,6 @@
 declare(strict_types=1);
 
 return [
-    'next'     => 'Następna &raquo;',
-    'previous' => '&laquo; Poprzednia',
+    'next'     => 'Next &raquo;',
+    'previous' => '&laquo; Previous',
 ];
