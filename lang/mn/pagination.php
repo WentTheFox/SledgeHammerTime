8492da4cd3aaba691cc3e54680ad1d508fc107b9
Double Colon <wentthefox@protonmail.com>
2025-11-19 02:16:08 +0100

    New translations pagination.php (Mongolian)

--- lang/mn/pagination.php
+++ lang/mn/pagination.php
@@ -3,6 +3,6 @@
 declare(strict_types=1);
 
 return [
-    'next'     => 'Дараах &raquo;',
-    'previous' => '&laquo; Өмнөх',
+    'next'     => 'Next &raquo;',
+    'previous' => '&laquo; Previous',
 ];
