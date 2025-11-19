6302c99494ccbedd001e2baaf50f33d1bba22817
Double Colon <wentthefox@protonmail.com>
2025-11-19 02:13:42 +0100

    New translations auth.php (Catalan)

--- lang/ca/auth.php
+++ lang/ca/auth.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 return [
-    'failed'   => 'Aquestes credencials no concorden amb els nostres registres.',
-    'password' => 'La contrasenya és incorrecta.',
-    'throttle' => 'Ha superat el nombre màxim d\'intents d\'accés. Si us plau, torni a intentar-ho en :seconds segons.',
+    'failed'   => 'These credentials do not match our records.',
+    'password' => 'The provided password is incorrect.',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 ];
