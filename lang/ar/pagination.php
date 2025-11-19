32558f9f08b8f42a1003674956729635a9bba0a7
Double Colon <wentthefox@protonmail.com>
2025-11-19 02:15:55 +0100

    New translations pagination.php (Arabic)

--- lang/ar/pagination.php
+++ lang/ar/pagination.php
@@ -3,6 +3,6 @@
 declare(strict_types=1);
 
 return [
-    'next'     => 'التالي &raquo;',
-    'previous' => '&laquo; السابق',
+    'next'     => 'Next &raquo;',
+    'previous' => '&laquo; Previous',
 ];
