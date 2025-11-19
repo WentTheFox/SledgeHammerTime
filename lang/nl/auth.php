57c3fec26ae9de54be82ae552e71a73f0df894d4
Double Colon <wentthefox@protonmail.com>
2025-11-19 02:13:55 +0100

    New translations auth.php (Dutch)

--- lang/nl/auth.php
+++ lang/nl/auth.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 return [
-    'failed'   => 'Deze combinatie van e-mailadres en wachtwoord is niet geldig.',
-    'password' => 'Wachtwoord is onjuist.',
-    'throttle' => 'Te veel mislukte aanmeldpogingen. Probeer het nog eens over :seconds seconden.',
+    'failed'   => 'These credentials do not match our records.',
+    'password' => 'The provided password is incorrect.',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 ];
