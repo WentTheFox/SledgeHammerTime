511fa130a289d707a1967dd60e5d3047db9f8bb5
Double Colon <wentthefox@protonmail.com>
2025-11-19 02:13:53 +0100

    New translations auth.php (Lithuanian)

--- lang/lt/auth.php
+++ lang/lt/auth.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 return [
-    'failed'   => 'Prisijungimo duomenys neatitinka.',
-    'password' => 'Slaptažodis neteisingas.',
-    'throttle' => 'Per daug bandymų prisijungti. Bandykite po :seconds sec.',
+    'failed'   => 'These credentials do not match our records.',
+    'password' => 'The provided password is incorrect.',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 ];
