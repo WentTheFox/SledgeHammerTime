39b38b0f2440c2bb510d292c8067622b288a0b82
Double Colon <wentthefox@protonmail.com>
2025-11-19 02:13:44 +0100

    New translations auth.php (Danish)

--- lang/da/auth.php
+++ lang/da/auth.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 return [
-    'failed'   => 'De angivne oplysninger er ugyldige.',
-    'password' => 'Adgangskoden er forkert.',
-    'throttle' => 'For mange loginforsøg. Prøv igen om :seconds sekunder.',
+    'failed'   => 'These credentials do not match our records.',
+    'password' => 'The provided password is incorrect.',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 ];
