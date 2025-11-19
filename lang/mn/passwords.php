45fe9059602e69d33781f0e054ebe2d015f795ae
Double Colon <wentthefox@protonmail.com>
2025-11-19 02:16:45 +0100

    New translations passwords.php (Mongolian)

--- lang/mn/passwords.php
+++ lang/mn/passwords.php
@@ -3,9 +3,9 @@
 declare(strict_types=1);
 
 return [
-    'reset'     => 'Таний нууц үг шинэчлэгдсэн!',
-    'sent'      => 'Нууц үг сэргээх холбоосийг таний и-мэйл хаяг уруу явуулсан!',
-    'throttled' => 'Татаж өмнө хүлээх уу.',
-    'token'     => 'Алдаатай нууц үг сэргээх холбоос.',
-    'user'      => 'Ийм и-мэйл хаягтай хэрэглэгч олдсонгүй.',
+    'reset'     => 'Your password has been reset.',
+    'sent'      => 'We have emailed your password reset link.',
+    'throttled' => 'Please wait before retrying.',
+    'token'     => 'This password reset token is invalid.',
+    'user'      => 'We can\'t find a user with that email address.',
 ];
