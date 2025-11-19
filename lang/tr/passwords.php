4417058b6d8a2082cd7f406aed51ea2dc17eb741
Double Colon <wentthefox@protonmail.com>
2025-11-19 02:16:51 +0100

    New translations passwords.php (Turkish)

--- lang/tr/passwords.php
+++ lang/tr/passwords.php
@@ -3,9 +3,9 @@
 declare(strict_types=1);
 
 return [
-    'reset'     => 'Parolanız sıfırlandı!',
-    'sent'      => 'Parola sıfırlama bağlantınız e-posta ile gönderildi!',
-    'throttled' => 'Tekrar denemeden önce lütfen bekleyin.',
-    'token'     => 'Parola sıfırlama kodu geçersiz.',
-    'user'      => 'Bu e-posta adresi ile kayıtlı bir üye bulunamadı.',
+    'reset'     => 'Your password has been reset.',
+    'sent'      => 'We have emailed your password reset link.',
+    'throttled' => 'Please wait before retrying.',
+    'token'     => 'This password reset token is invalid.',
+    'user'      => 'We can\'t find a user with that email address.',
 ];
