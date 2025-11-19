5d7127a8a6b73b1d0399fe582d924d422563eb6e
Double Colon <wentthefox@protonmail.com>
2025-11-19 02:16:26 +0100

    New translations pagination.php (Hindi)

--- lang/hi/pagination.php
+++ lang/hi/pagination.php
@@ -3,6 +3,6 @@
 declare(strict_types=1);
 
 return [
-    'next'     => 'अगला &raquo;',
-    'previous' => '&laquo; पिछला',
+    'next'     => 'Next &raquo;',
+    'previous' => '&laquo; Previous',
 ];
