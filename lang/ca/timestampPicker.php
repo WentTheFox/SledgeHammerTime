ac3a8aff311960f47e4eb8a9a3ce0085d653fb7f
WentTheFox <wentthefox@protonmail.com>
2026-01-03 04:13:03 +0100

    New translations timestamppicker.php (Catalan)

--- lang/ca/timestampPicker.php
+++ lang/ca/timestampPicker.php
@@ -8,6 +8,7 @@ return [
       'time' => 'Hora',
       'dateAndTime' => 'Data i hora',
       'timezone' => 'Fus horari',
+      'naturalLanguageInput' => '@time input',
       'modeOffset' => 'Absolute Offset',
       'modeZoneName' => 'Zone Name',
     ],
@@ -23,6 +24,9 @@ return [
       'nextMonth' => 'Next month',
       'nextYear' => 'Next year',
     ],
+    'validation' => [
+      'naturalLanguageParseError' => 'Could not parse natural language input'
+    ]
   ],
   'table' => [
     'syntaxColumn' => 'Sintaxi al xat',
