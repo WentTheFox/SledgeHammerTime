e0ec8e53f2c247a4eb171b0a64f87d7b3c588252
WentTheFox <wentthefox@protonmail.com>
2026-01-03 04:12:24 +0100

    New translations global.php (Czech)

--- lang/cs/global.php
+++ lang/cs/global.php
@@ -32,6 +32,10 @@ return [
   'sidebar' => [
     'inputSettings' => [
       'title' => 'Nastavení vstupu',
+      'naturalLanguageInput' => [
+        'label' => '@time input field',
+        'description' => 'Allow manipulating the selected timestamp with natural language (e.g. “in 5 hours”) via an additional free-text input. Similar to the new Discord @time tag. Only a limited number of languages are supported.',
+      ],
       'customDateInput' => [
         'label' => 'Formátované vstupní pole data',
         'description' => "Nahradí výchozí vstupní pole prohlížeče formátovaným.",
@@ -87,6 +91,7 @@ return [
       'vueJs' => 'Vue.js',
       'dateFns' => 'date-fns',
       'vueTippy' => 'VueTippy',
+      'chrono' => 'chrono',
       'translatedBy' => 'Přeložili <1></1>',
       'openSourceSoftware' => 'Otevřený software',
       'viewSourceCode' => 'Zobrazit zdrojový kód',
