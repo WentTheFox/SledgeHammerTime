6e2ceb8ff2fb589fdfeae9c95886b4e202cd19f6
WentTheFox <wentthefox@protonmail.com>
2026-01-03 04:13:02 +0100

    New translations timestamppicker.php (Bulgarian)

--- lang/bg/timestampPicker.php
+++ lang/bg/timestampPicker.php
@@ -8,6 +8,7 @@ return [
       'time' => 'Час',
       'dateAndTime' => 'Дата и час',
       'timezone' => 'Часова зона',
+      'naturalLanguageInput' => '@time input',
       'modeOffset' => 'Absolute Offset',
       'modeZoneName' => 'Zone Name',
     ],
@@ -23,6 +24,9 @@ return [
       'nextMonth' => 'Next month',
       'nextYear' => 'Next year',
     ],
+    'validation' => [
+      'naturalLanguageParseError' => 'Could not parse natural language input'
+    ]
   ],
   'table' => [
     'syntaxColumn' => 'Синтаксис на чата',
