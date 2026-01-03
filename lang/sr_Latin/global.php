e1ed69788798e4ea5d156a9a73602bc059f2f782
WentTheFox <wentthefox@protonmail.com>
2026-01-03 04:12:57 +0100

    New translations global.php (Serbian (Latin))

--- lang/sr_Latin/global.php
+++ lang/sr_Latin/global.php
@@ -32,6 +32,10 @@ return [
   'sidebar' => [
     'inputSettings' => [
       'title' => 'Podešavanja unosa',
+      'naturalLanguageInput' => [
+        'label' => '@time input field',
+        'description' => 'Allow manipulating the selected timestamp with natural language (e.g. “in 5 hours”) via an additional free-text input. Similar to the new Discord @time tag. Only a limited number of languages are supported.',
+      ],
       'customDateInput' => [
         'label' => 'Custom date input',
         'description' => "Replace the browser default date input with a custom one.",
@@ -87,6 +91,7 @@ return [
       'vueJs' => 'Vue.js',
       'dateFns' => 'date-fns',
       'vueTippy' => 'VueTippy',
+      'chrono' => 'chrono',
       'translatedBy' => 'Prevod <1></1>',
       'openSourceSoftware' => 'Softver otvorenog koda',
       'viewSourceCode' => 'Pogledajte izvorni kod',
