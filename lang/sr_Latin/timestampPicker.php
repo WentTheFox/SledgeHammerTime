1f3f381d173be07b82351cb60d393376973e5536
WentTheFox <wentthefox@protonmail.com>
2026-01-03 04:13:35 +0100

    New translations timestamppicker.php (Serbian (Latin))

--- lang/sr_Latin/timestampPicker.php
+++ lang/sr_Latin/timestampPicker.php
@@ -8,6 +8,7 @@ return [
       'time' => 'Vreme',
       'dateAndTime' => 'Datum i vreme',
       'timezone' => 'Vremenska zona',
+      'naturalLanguageInput' => '@time input',
       'modeOffset' => 'Absolute Offset',
       'modeZoneName' => 'Zone Name',
     ],
@@ -23,6 +24,9 @@ return [
       'nextMonth' => 'Next month',
       'nextYear' => 'Next year',
     ],
+    'validation' => [
+      'naturalLanguageParseError' => 'Could not parse natural language input'
+    ]
   ],
   'table' => [
     'syntaxColumn' => 'Sintaksa u poruci',
