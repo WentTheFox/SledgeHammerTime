ed67defca03124a3814c062f86129942ad855e63
WentTheFox <wentthefox@protonmail.com>
2026-01-03 04:13:20 +0100

    New translations timestamppicker.php (Ukrainian)

--- lang/uk/timestampPicker.php
+++ lang/uk/timestampPicker.php
@@ -8,6 +8,7 @@ return [
       'time' => 'Час',
       'dateAndTime' => 'Дата і час',
       'timezone' => 'Часовий пояс',
+      'naturalLanguageInput' => '@time input',
       'modeOffset' => 'Absolute Offset',
       'modeZoneName' => 'Zone Name',
     ],
@@ -23,6 +24,9 @@ return [
       'nextMonth' => 'Next month',
       'nextYear' => 'Next year',
     ],
+    'validation' => [
+      'naturalLanguageParseError' => 'Could not parse natural language input'
+    ]
   ],
   'table' => [
     'syntaxColumn' => 'Синтаксис чату',
