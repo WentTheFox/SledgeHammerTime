858f971afa6b59427915af344033ceb64ef491d6
WentTheFox <wentthefox@protonmail.com>
2026-01-03 19:59:07 +0100

    New translations timestamppicker.php (Malay)

--- lang/ms/timestampPicker.php
+++ lang/ms/timestampPicker.php
@@ -14,8 +14,7 @@ return [
     ],
     'button' => [
       'jumpToToday' => 'Lompat ke bulan semasa',
-      'contextRangeLong' => '<0/> – <2/>',
-      'contextRangeShort' => '<0/>–<2/>',
+      'contextRange' => '<0/>–<2/>',
     ],
     'tooltip' => [
       'setToCurrent' => 'Tetapkan ke masa semasa',
