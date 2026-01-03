ec1fe1802e8567b18d4a292f1870869dd2060470
WentTheFox <wentthefox@protonmail.com>
2026-01-03 04:12:35 +0100

    New translations global.php (Lithuanian)

--- lang/lt/global.php
+++ lang/lt/global.php
@@ -32,6 +32,10 @@ return [
   'sidebar' => [
     'inputSettings' => [
       'title' => 'Įvedimo nustatymai',
+      'naturalLanguageInput' => [
+        'label' => '@time input field',
+        'description' => 'Allow manipulating the selected timestamp with natural language (e.g. “in 5 hours”) via an additional free-text input. Similar to the new Discord @time tag. Only a limited number of languages are supported.',
+      ],
       'customDateInput' => [
         'label' => 'Custom date input',
         'description' => "Replace the browser default date input with a custom one.",
@@ -87,6 +91,7 @@ return [
       'vueJs' => 'Vue.js',
       'dateFns' => 'date-fns',
       'vueTippy' => 'VueTippy',
+      'chrono' => 'chrono',
       'translatedBy' => 'Išvertė <1></1>',
       'openSourceSoftware' => 'Atvirojo kodo programa',
       'viewSourceCode' => 'Peržiūrėti išeitinį kodą',
