522d06cef5fa30eba576a95bed37552e6f55a1aa
WentTheFox <wentthefox@protonmail.com>
2026-01-03 04:13:05 +0100

    New translations timestamppicker.php (Danish)

--- lang/da/timestampPicker.php
+++ lang/da/timestampPicker.php
@@ -8,6 +8,7 @@ return [
       'time' => 'Tid',
       'dateAndTime' => 'Dato og tidspunkt',
       'timezone' => 'Tidszone',
+      'naturalLanguageInput' => '@time input',
       'modeOffset' => 'Absolute Offset',
       'modeZoneName' => 'Zone Name',
     ],
@@ -23,6 +24,9 @@ return [
       'nextMonth' => 'Next month',
       'nextYear' => 'Next year',
     ],
+    'validation' => [
+      'naturalLanguageParseError' => 'Could not parse natural language input'
+    ]
   ],
   'table' => [
     'syntaxColumn' => 'Syntaks',
