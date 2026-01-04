59a4c762d7865ba08f52a17d95ae54dba34090cc
WentTheFox <wentthefox@protonmail.com>
2026-01-04 22:45:25 +0100

    New translations timestamppicker.php (Japanese)

--- lang/ja/timestampPicker.php
+++ lang/ja/timestampPicker.php
@@ -47,7 +47,7 @@ return [
     ],
     'oldSite' => [
       'header' => '旧Hammer Timeサイト',
-      'p' => 'ベータ版が公開されるまで引き続き利用可能な、プロジェクトの旧ウェブサイト',
+      'p' => 'The old website of the project, still available until further notice',
     ],
     'textColor' => [
       'header' => 'Rebaneの<1>色付き</1>テキストジェネレーター',
