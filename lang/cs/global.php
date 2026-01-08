eb966a8c1e55e5ca59f224f6b638599da32a6633
WentTheFox <wentthefox@protonmail.com>
2026-01-08 20:44:01 +0100

    New translations global.php (Czech)

--- lang/cs/global.php
+++ lang/cs/global.php
@@ -53,8 +53,8 @@ return [
         'description' => "Odstraní stíny a výškové efekty u vstupních polí a tlačítek",
       ],
       'hourCycle' => [
-        'label' => 'Time input clock type',
-        'description' => 'Change how hours should be displayed in the time input',
+        'label' => 'Custom time input clock type',
+        'description' => 'Change how hours should be displayed in the custom time input. Has no effect on the previews in the example results column.',
         'options' => [
           'default' => 'Language default',
           'h12' => '12-hour',
