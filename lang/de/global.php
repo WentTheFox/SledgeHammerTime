1201a67a362365bc7e96f00d208b057dbe96be74
WentTheFox <wentthefox@protonmail.com>
2026-01-08 20:35:16 +0100

    New translations global.php (German)

--- lang/de/global.php
+++ lang/de/global.php
@@ -52,6 +52,15 @@ return [
         'label' => 'Abgeflachte Oberfläche',
         'description' => "Deaktiviert Schatten- und Höheneffekte bei Eingabefeldern und Buttons",
       ],
+      'hourCycle' => [
+        'label' => 'Time input clock type',
+        'description' => 'Change how hours should be displayed in the time input',
+        'options' => [
+          'default' => 'Language default',
+          'h12' => '12-hour',
+          'h24' => '24-hour',
+        ],
+      ],
     ],
     'timeSync' => [
       'title' => 'Zeitsynchronisation',
