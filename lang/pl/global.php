80c8ff1b1b8c69c6a774cbd7dfe2a5effc3083f7
WentTheFox <wentthefox@protonmail.com>
2026-01-08 20:35:27 +0100

    New translations global.php (Polish)

--- lang/pl/global.php
+++ lang/pl/global.php
@@ -52,6 +52,15 @@ return [
         'label' => 'Wyrównaj interfejs',
         'description' => "Wyłącz efekt cienia i wysokości na polach tekstowych i przyciskach",
       ],
+      'hourCycle' => [
+        'label' => 'Time input clock type',
+        'description' => 'Change how hours should be displayed in the time input',
+        'options' => [
+          'default' => 'Language default',
+          'h12' => '12-hour',
+          'h24' => '24-hour',
+        ],
+      ],
     ],
     'timeSync' => [
       'title' => 'Synchronizacja czasu',
