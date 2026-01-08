05020ddb339783b96eb28b6887a14a79a4eac468
WentTheFox <wentthefox@protonmail.com>
2026-01-08 20:35:17 +0100

    New translations global.php (Greek)

--- lang/el/global.php
+++ lang/el/global.php
@@ -52,6 +52,15 @@ return [
         'label' => 'Επιπεδοποίηση διεπαφής',
         'description' => "Απενεργοποίηση εφέ σκίασης και ύψους στις εισόδους και τα κουμπιά",
       ],
+      'hourCycle' => [
+        'label' => 'Time input clock type',
+        'description' => 'Change how hours should be displayed in the time input',
+        'options' => [
+          'default' => 'Language default',
+          'h12' => '12-hour',
+          'h24' => '24-hour',
+        ],
+      ],
     ],
     'timeSync' => [
       'title' => 'Συγχρονισμός Ώρας',
