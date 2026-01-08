aad2c93423542bfb88cd92dabf238bdd3a3d9a58
WentTheFox <wentthefox@protonmail.com>
2026-01-08 20:35:09 +0100

    New translations global.php (French)

--- lang/fr/global.php
+++ lang/fr/global.php
@@ -52,6 +52,15 @@ return [
         'label' => 'Interface aplanie',
         'description' => "Désactiver les effets d'ombre et de hauteur sur les entrées et les boutons",
       ],
+      'hourCycle' => [
+        'label' => 'Time input clock type',
+        'description' => 'Change how hours should be displayed in the time input',
+        'options' => [
+          'default' => 'Language default',
+          'h12' => '12-hour',
+          'h24' => '24-hour',
+        ],
+      ],
     ],
     'timeSync' => [
       'title' => 'Synchronisation de l\'heure',
