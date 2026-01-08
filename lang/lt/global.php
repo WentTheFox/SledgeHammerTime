4be147804fc3189bf2cb9bc88b6f6db441641428
WentTheFox <wentthefox@protonmail.com>
2026-01-08 20:44:13 +0100

    New translations global.php (Lithuanian)

--- lang/lt/global.php
+++ lang/lt/global.php
@@ -53,8 +53,8 @@ return [
         'description' => "Disable shadow and height effects on inputs and buttons",
       ],
       'hourCycle' => [
-        'label' => 'Time input clock type',
-        'description' => 'Change how hours should be displayed in the time input',
+        'label' => 'Custom time input clock type',
+        'description' => 'Change how hours should be displayed in the custom time input. Has no effect on the previews in the example results column.',
         'options' => [
           'default' => 'Language default',
           'h12' => '12-hour',
