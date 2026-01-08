5d690cbd029a2fa3035f50c25e5a4e38ed06d453
WentTheFox <wentthefox@protonmail.com>
2026-01-08 20:35:32 +0100

    New translations global.php (Turkish)

--- lang/tr/global.php
+++ lang/tr/global.php
@@ -52,6 +52,15 @@ return [
         'label' => 'Düzleştirilmiş arayüz',
         'description' => "Girişler ve butonlardaki gölge ve yükseklik efektlerini devre dışı bırakın",
       ],
+      'hourCycle' => [
+        'label' => 'Time input clock type',
+        'description' => 'Change how hours should be displayed in the time input',
+        'options' => [
+          'default' => 'Language default',
+          'h12' => '12-hour',
+          'h24' => '24-hour',
+        ],
+      ],
     ],
     'timeSync' => [
       'title' => 'Zaman Senkronizasyonu',
