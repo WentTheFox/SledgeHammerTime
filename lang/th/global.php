42e7fc41f67bfd8ab8e36aec47983dcd70671b6c
WentTheFox <wentthefox@protonmail.com>
2026-01-09 01:32:40 +0100

    New translations global.php (Thai)

--- lang/th/global.php
+++ lang/th/global.php
@@ -61,6 +61,7 @@ return [
           'h24' => '24-hour',
         ],
       ],
+      'advancedSettings' => 'Advanced input settings',
     ],
     'timeSync' => [
       'title' => 'การทำให้ข้อมูลตรงกัน',
