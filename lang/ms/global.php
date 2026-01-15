96f729bd57e13695219785d47b2b980158bede5c
WentTheFox <wentthefox@protonmail.com>
2026-01-15 01:34:29 +0100

    New translations global.php (Malay)

--- lang/ms/global.php
+++ lang/ms/global.php
@@ -61,6 +61,13 @@ return [
           'h24' => '24 jam',
         ],
       ],
+      'firstDayOfWeek' => [
+        'label' => 'First day of the week',
+        'description' => 'Change which day should be considered the first day of the week in the custom date input.',
+        'options' => [
+          'default' => 'Language default',
+        ],
+      ],
       'advancedSettings' => 'Tetapan input lanjutan',
     ],
     'timeSync' => [
