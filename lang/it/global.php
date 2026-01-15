20e397c14b0fddf584a9c11318d6180e3b148f9e
WentTheFox <wentthefox@protonmail.com>
2026-01-15 01:34:06 +0100

    New translations global.php (Italian)

--- lang/it/global.php
+++ lang/it/global.php
@@ -61,6 +61,13 @@ return [
           'h24' => '24 ore',
         ],
       ],
+      'firstDayOfWeek' => [
+        'label' => 'First day of the week',
+        'description' => 'Change which day should be considered the first day of the week in the custom date input.',
+        'options' => [
+          'default' => 'Language default',
+        ],
+      ],
       'advancedSettings' => 'Impostazioni di input avanzate',
     ],
     'timeSync' => [
