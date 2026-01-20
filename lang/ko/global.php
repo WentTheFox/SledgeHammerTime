9546906f7aec4b5f04d9943d8165a693a792b151
WentTheFox <wentthefox@protonmail.com>
2026-01-20 05:24:52 +0100

    New translations global.php (Korean)

--- lang/ko/global.php
+++ lang/ko/global.php
@@ -8,13 +8,13 @@ return [
     "content" => "표시할 콘텐츠가 없습니다",
   ],
   "maintenanceMode" => [
-    "heading" => "Maintenance Mode",
+    "heading" => "관리 모드",
     "content" => [
-      'pleaseWait' => "The application is currently being updated, please try again in a few seconds.",
-      'joinSupportServer' => "If the issue persists for more than a few minutes, please join the Discord support server and let us know.",
-      'contactDeveloper' => "If the issue persists for more than a few minutes, please contact the developer and let them know.",
+      'pleaseWait' => "현재 업데이트가 진행 중 입니다. 몇 초 후에 다시 시도해 주세요.",
+      'joinSupportServer' => "문제가 수 분 동안 계속 된다면, Discord 지원 서버에 참가 후에 문의 해 주세요.",
+      'contactDeveloper' => "문제가 수 분 동안 계속 된다면, 개발자에게 연락하여 문제를 알려주세요.",
     ],
-    "supportServerButton" => "Support Server",
+    "supportServerButton" => "지원 서버",
   ],
   'incompleteTranslations' => '번역 미완료',
   'contributeTranslations' => '기여하기',
@@ -28,7 +28,7 @@ return [
     'botSettings' => '앱 설정',
     'profile' => '프로필',
     'legal' => '법률 정보',
-    'analytics' => 'Analytics',
+    'analytics' => '분석',
   ],
   'sidebar' => [
     'inputSettings' => [
@@ -38,12 +38,12 @@ return [
         'description' => 'Allow manipulating the selected timestamp with natural language (e.g. “in 5 hours”) via an additional free-text input. Similar to the new Discord @time tag. Only a limited number of languages are supported.',
       ],
       'customDateInput' => [
-        'label' => 'Custom date input',
-        'description' => "Replace the browser default date input with a custom one.",
+        'label' => '사용자 날짜 입력',
+        'description' => "브라우저의 기본 날짜를 사용자 지정 날짜로 변경합니다.",
       ],
       'customTimeInput' => [
-        'label' => 'Custom time input',
-        'description' => "Replace the browser default time input with a custom one. This is especially useful if you have trouble selecting the time on mobile browsers.",
+        'label' => '사용자 시간 입력',
+        'description' => "브라우저의 기본 시간 입력을 사용자 지정 시간으로 변경합니다. 모바일 브라우저에서 시간 선택에 문제가 있는 경우에 유용합니다.",
       ],
       'separateInputs' => [
         'label' => '별개 입력',
@@ -51,25 +51,25 @@ return [
       ],
       'flatUi' => [
         'label' => 'Flatten interface',
-        'description' => "Disable shadow and height effects on inputs and buttons",
+        'description' => "입력 창과 버튼의 그림자와 높이 효과 비활성화",
       ],
       'hourCycle' => [
-        'label' => 'Custom time input clock type',
-        'description' => 'Change how hours should be displayed in the custom time input. Has no effect on the previews in the example results column.',
+        'label' => '사용자 시간 입력 시간 유형',
+        'description' => '사용자 시간 입력 창에 시간을 표시하는 방법을 변경합니다. 미리 보기 열에는 영향이 없습니다.',
         'options' => [
-          'default' => 'Language default',
-          'h12' => '12-hour',
-          'h24' => '24-hour',
+          'default' => '기본 언어',
+          'h12' => '12시간',
+          'h24' => '24시간',
         ],
       ],
       'firstDayOfWeek' => [
-        'label' => 'First day of the week',
-        'description' => 'Change which day should be considered the first day of the week in the calendar of the custom date input.',
+        'label' => '일주일의 첫날',
+        'description' => '사용자 시간 입력 창의 일주일의 첫 번째 날이 무슨 요일인지 변경합니다.',
         'options' => [
-          'default' => 'Language default',
+          'default' => '기본 언어',
         ],
       ],
-      'advancedSettings' => 'Advanced input settings',
+      'advancedSettings' => '고급 입력 설정',
     ],
     'timeSync' => [
       'title' => '시간 동기화',
@@ -77,7 +77,7 @@ return [
         'syncing' => '당신의 시스템 시간이 우리의 서버와 동기화 중 입니다. 
 잠시만 기다려 주십시오.',
         'accurate' => '당신의 시스템 시각은 정확합니다.',
-        'potentiallyWrong' => 'Your system clock might be wrong.',
+        'potentiallyWrong' => '시스템 시간이 틀릴 수 있습니다.',
         'value' => '현재 지역 시간과 서버 시간 사이의 차이는 :offset입니다.',
       ],
       'autoTimeSync' => [
