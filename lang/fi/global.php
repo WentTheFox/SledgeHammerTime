c354992ce9a498ab05b422a80401d567711310e6
WentTheFox <wentthefox@protonmail.com>
2026-01-20 03:45:19 +0100

    New translations global.php (Finnish)

--- lang/fi/global.php
+++ lang/fi/global.php
@@ -28,6 +28,7 @@ return [
     'botSettings' => 'App Settings',
     'profile' => 'Profiili',
     'legal' => 'Legal Information',
+    'analytics' => 'Analytics',
   ],
   'sidebar' => [
     'inputSettings' => [
