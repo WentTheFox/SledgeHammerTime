4a22d731871abe2d21b1c450e018cbac252bc872
WentTheFox <wentthefox@protonmail.com>
2026-01-20 05:26:33 +0100

    New translations analytics.php (Korean)

--- lang/ko/analytics.php
+++ lang/ko/analytics.php
@@ -4,6 +4,7 @@ return [
   'heading' => 'Page View Analytics',
   'description' => 'This page contains basic analytics (both stored and displayed without any personally identifiable details) for aggregate total page views in the app over the last 30 days.',
   'collectionMethod' => 'The data is collected on the server side, based on responses sent to a limited set of pages. The page views are recorded individually but aggregated daily by a background process.',
+  'lastUpdated' => 'The information on this page is cached for a short period to reduce server load. The data you see was last updated <1/>.',
   'charts' => [
     'dailyTotal' => 'Daily Total Page Views',
     'breakdown' => 'Page Views Breakdown',
