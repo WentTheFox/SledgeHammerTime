94adb2592ac785c3c56fdec9881eff904d766fcc
WentTheFox <wentthefox@protonmail.com>
2026-01-26 11:24:48 +0100

    New translations analytics.php (Russian)

--- lang/ru/analytics.php
+++ lang/ru/analytics.php
@@ -1,18 +1,18 @@
 <?php
 
 return [
-  'heading' => 'Page View Analytics',
+  'heading' => 'Аналитика просмотров страниц',
   'description' => 'This page contains basic analytics (both stored and displayed without any personally identifiable details) for aggregate total page views in the app over the last 30 days.',
-  'collectionMethod' => 'The data is collected on the server side, based on responses sent to a limited set of pages. The page views are recorded individually but aggregated daily by a background process.',
+  'collectionMethod' => 'Данные собираются на стороне сервера, основанного на ответах отправленных определённому набору страниц. Просмотры страниц записаны индивидуально, но собраны ежедневно фоновым процессом.',
   'lastUpdated' => 'The information on this page is cached for a short period to reduce server load. The data you see was last updated <1/>.',
   'charts' => [
-    'dailyTotal' => 'Daily Total Page Views',
-    'breakdown' => 'Page Views Breakdown',
-    'byPage' => 'By Pages',
-    'byLanguage' => 'By Language',
-    'hideUnknown' => 'Hide unknown values',
+    'dailyTotal' => 'Общие ежедневные просмотры страниц',
+    'breakdown' => 'Разбивка отображения страниц',
+    'byPage' => 'По страницам',
+    'byLanguage' => 'По языкам',
+    'hideUnknown' => 'Скрыть неизвестные значения',
   ],
   'values' => [
-    'unknown' => 'Unknown',
+    'unknown' => 'Неизвестный',
   ],
 ];
