9f3731b0bf63c7ce78699c2cba0ed74d0cb2d1bb
WentTheFox <wentthefox@protonmail.com>
2026-02-14 20:29:36 +0100

    New translations analytics.php (Polish)

--- lang/pl/analytics.php
+++ lang/pl/analytics.php
@@ -3,11 +3,11 @@
 return [
   'heading' => 'Analiza wejść na stronę',
   'description' => 'Ta strona zawiera podstawowe dane analityczne (zarówno przechowywane, jak i wyświetlane bez żadnych danych umożliwiających identyfikację użytkownika) dotyczące łącznej liczby odsłon w aplikacji w ciągu ostatnich 30 dni.',
-  'collectionMethod' => 'The data is collected on the server side, based on responses sent to a limited set of pages. The page views are recorded individually but aggregated daily by a background process.',
+  'collectionMethod' => 'Dane są gromadzone po stronie serwera, na podstawie odpowiedzi wysyłanych do ograniczonej ilości stron. Wyświetlenia stron są odnotowywane indywidualnie, ale agregowane codziennie przez proces w tle.',
   'lastUpdated' => 'Informacje na tej stronie są buforowane przez krótki czas, aby zmniejszyć obciążenie serwera. Widoczne dane zostały ostatnio zaktualizowane <1/>.',
   'charts' => [
     'dailyTotal' => 'Dzienna łączna liczba odsłon',
-    'breakdown' => 'Page Views Breakdown',
+    'breakdown' => 'Podział wyświetleń strony',
     'byPage' => 'Według stron',
     'byLanguage' => 'Według języka',
   ],
