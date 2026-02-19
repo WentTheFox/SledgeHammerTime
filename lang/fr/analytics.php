1a5cfd006f2a1f58e694e5399fde6e5e837f8e6d
WentTheFox <wentthefox@protonmail.com>
2026-02-19 11:11:26 +0100

    New translations analytics.php (French)

--- lang/fr/analytics.php
+++ lang/fr/analytics.php
@@ -3,7 +3,7 @@
 return [
   'heading' => 'Statistiques des pages vues',
   'description' => 'This page contains basic analytics (both stored and displayed without any personally identifiable details) for aggregate total page views in the app over the last 30 days.',
-  'collectionMethod' => 'Les données collectes du côté de ce serveur sont basés sur des réponses envoyer a une limite de page défini. Les pages vu filmer individuellement mais accepter régulièrement par un processeur d\'arrière-plan.',
+  'collectionMethod' => 'Les données sont collectées côté serveur, basé sur des réponses envoyées vers un nombre limité de pages. Les pages consultées sont enregistrées individuellement mais regroupées quotidiennement par un processus en arrière-plan.',
   'lastUpdated' => 'The information on this page is cached for a short period to reduce server load. The data you see was last updated <1/>.',
   'charts' => [
     'dailyTotal' => 'Daily Total Page Views',
