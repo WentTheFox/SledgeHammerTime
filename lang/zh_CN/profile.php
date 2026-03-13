f8881d04e7cdc135c23a0f43959bc64fcd951f83
WentTheFox <wentthefox@protonmail.com>
2026-03-13 02:35:38 +0100

    New translations profile.php (Chinese Simplified)

--- lang/zh_CN/profile.php
+++ lang/zh_CN/profile.php
@@ -2,40 +2,40 @@
 
 return [
   'title' => '个人资料',
-  'intro' => 'The profile page allows you to manage your account information specifically related to the website. If you are looking for where to change defaults and other settings for the Discord app, visit the <1>App Settings</1> page.',
+  'intro' => '个人资料页面允许您管理专门与本网站相关的账户信息。如果您正在寻找在哪里更改 Discord 应用的默认值和其他设置，请访问 <1>App Settings</1> 页面。',
   'information' => [
     'heading' => '个人资料信息',
     'description' => "更新您账户的个人资料信息。",
     'displayName' => '显示名称',
-    'hiddenFormats' => 'Formats hidden from picker table',
+    'hiddenFormats' => '从选择器表格中隐藏的格式',
     'saveSuccess' => '您个人资料的更改已保存。',
   ],
   'accounts' => [
     'heading' => '已关联账户',
-    'description' => 'You can link multiple accounts to your HammerTime account to be able to manage them in one place. The basic user information displayed here is automatically updated at most once an hour when you load this page.',
-    'discord' => 'Discord Accounts',
-    'crowdin' => 'Crowdin Accounts',
+    'description' => '您可以将多个账户关联到您的 HammerTime 账户，以便在一个地方进行管理。此处显示的基本用户信息在您加载此页面时最多每小时自动更新一次。',
+    'discord' => 'Discord 账号',
+    'crowdin' => 'Crowdin 账户',
     'noConnectedAccounts' => '您目前没有关联任何账户',
     'linkAdditional' => [
-      'heading' => 'Link Additional Accounts',
-      'description' => 'Use the buttons below to connect additional accounts to your current HammerTime account.',
+      'heading' => '关联更多账户',
+      'description' => '使用下方按钮将更多账户关联到您当前的 HammerTime 账户。',
       'discord' => [
-        'description' => 'If your profile picture or display name changed, you can also use this to update that information for any Discord account which is already linked.',
+        'description' => '如果您的头像或显示名称发生了变化，您也可以通过此操作来更新已关联的 Discord 账户的相关信息。',
         'authorize' => '授权 Discord 账户',
       ],
       'crowdin' => [
-        'description' => 'If you are a translator for the project, linking your Crowdin account will eventually let you customize how your name appears in the translation credits, and also serves as an extra level of verification that these accounts belong to you in case you join the project\'s Discord server.',
-        'authorize' => 'Authorize Crowdin account',
+        'description' => '如果您是该项目的翻译者，关联您的 Crowdin 账户最终将允许您自定义姓名在翻译贡献名单中的显示方式，同时在您加入该项目的 Discord 服务器时，这还可以作为额外的身份验证，证明这些账户属于您。',
+        'authorize' => '授权 Crowdin 账户',
       ],
     ],
   ],
   'deletion' => [
-    'heading' => 'Account Deletion',
-    'description' => 'Once your account is deleted, all of its resources and data will be permanently deleted. Before deleting your account, please download any data or information that you wish to retain.',
-    'deleteButton' => 'Delete Account',
+    'heading' => '删除帐户',
+    'description' => '账户一旦删除，其所有资源和数据都将被永久删除。在删除账户之前，请下载您希望保留的任何数据或信息。',
+    'deleteButton' => '删除帐户',
     'confirmDialog' => [
-      'header' => 'Are you sure you want to delete your account?',
-      'body' => 'Once your account is deleted, all of its resources and data will be permanently deleted.',
+      'header' => '确定要删除您的账号吗？',
+      'body' => '一旦您的账户被删除，其所有资源和数据都将被永久删除。',
     ],
   ],
 ];
