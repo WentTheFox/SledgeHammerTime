3a32c6c1df193f7aae6830359666fd6dcbc2b852
WentTheFox <wentthefox@protonmail.com>
2026-04-04 06:50:21 +0200

    New translations profile.php (Norwegian Bokmal)

--- lang/nb/profile.php
+++ lang/nb/profile.php
@@ -24,7 +24,7 @@ return [
         'authorize' => 'Autorisere Discord konto',
       ],
       'crowdin' => [
-        'description' => 'If you are a translator for the project, linking your Crowdin account will eventually let you customize how your name appears in the translation credits, and also serves as an extra level of verification that these accounts belong to you in case you join the project\'s Discord server.',
+        'description' => 'If you are a translator for the project, linking your Crowdin account lets you customize how your name appears in the translation credits, and also serves as an extra level of verification that these accounts belong to you in case you join the project\'s Discord server.',
         'authorize' => 'Autorisere Crowdin konto',
       ],
     ],
