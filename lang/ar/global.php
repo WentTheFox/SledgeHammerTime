f0281335a86006a559665494da04c53a8d4d1a60
WentTheFox <wentthefox@protonmail.com>
2026-04-05 04:34:04 +0200

    New translations global.php (Arabic)

--- lang/ar/global.php
+++ lang/ar/global.php
@@ -18,6 +18,11 @@ return [
   ],
   'incompleteTranslations' => 'الترجمة غير مكتملة',
   'contributeTranslations' => 'المشاركة في الترجمة',
+  'timezoneBadge' => [
+    'currently' => 'Observed currently',
+    'atPickedDate' => 'Observed at picked time',
+    'currentlyAndAtPickedDate' => 'Observed currently & at picked time',
+  ],
   'copyToClipboard' => 'نسخ إلى الحافظة',
   'copiedToClipboard' => 'Copied to clipboard!',
   'jsDisabled' => [
@@ -53,6 +58,10 @@ return [
       'flatUi' => [
         'label' => 'Flatten interface',
         'description' => "Disable shadow and height effects on inputs and buttons",
+      ],
+      'timezoneStickyHeaders' => [
+        'label' => 'Timezone group headers',
+        'description' => 'Show sticky group headers (e.g. America, Europe) in the timezone selector dropdown.',
       ],
       'hourCycle' => [
         'label' => 'Custom time input clock type',
