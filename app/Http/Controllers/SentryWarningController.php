33954d552f3d212dfecfbdfa1eaaf191ba07377a
WentTheFox <mail@went.tf>
2026-04-08 19:04:43 +0200

    Fix phpstan error in SentryWarningController

--- app/Http/Controllers/SentryWarningController.php
+++ app/Http/Controllers/SentryWarningController.php
@@ -2,15 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
 
-class SentryWarningController extends Controller
-{
-    public function acknowledge(Request $request)
-    {
-        Session::put('sentry_warning_acknowledged', true);
+class SentryWarningController extends Controller {
+  public function acknowledge(Request $request):RedirectResponse {
+    Session::put('sentry_warning_acknowledged', true);
 
-        return redirect()->intended('/');
-    }
+    return redirect()->intended('/');
+  }
 }
