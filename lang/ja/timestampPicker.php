0c33c9db24cc2a011ff6961eb9c5047292754c8a
WentTheFox <wentthefox@protonmail.com>
2026-04-10 01:30:28 +0200

    New translations timestamppicker.php (Japanese)

--- lang/ja/timestampPicker.php
+++ lang/ja/timestampPicker.php
@@ -34,6 +34,11 @@ return [
   'table' => [
     'syntaxColumn' => 'メッセージの構文',
     'resultColumn' => '例',
+    'editFormats' => 'Customize formats',
+    'resetFormats' => 'Reset to defaults',
+    'hideFormat' => 'Hide this format',
+    'showFormat' => 'Show this format',
+    'unhideInProfile' => 'Unhide in profile settings',
   ],
   'usefulLinks' => [
     'lead' => '関連するもの',
