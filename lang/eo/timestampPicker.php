486a0d0d6801e4678e89085e6560fef460838ddc
WentTheFox <wentthefox@protonmail.com>
2026-04-10 01:30:54 +0200

    New translations timestamppicker.php (Esperanto)

--- lang/eo/timestampPicker.php
+++ lang/eo/timestampPicker.php
@@ -34,6 +34,11 @@ return [
   'table' => [
     'syntaxColumn' => 'Babilsintakso',
     'resultColumn' => 'Ekzempla rezulto',
+    'editFormats' => 'Customize formats',
+    'resetFormats' => 'Reset to defaults',
+    'hideFormat' => 'Hide this format',
+    'showFormat' => 'Show this format',
+    'unhideInProfile' => 'Unhide in profile settings',
   ],
   'usefulLinks' => [
     'lead' => 'You may also find these useful:',
