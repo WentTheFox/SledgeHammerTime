850bf77d10d360656039b6bfd2edd7b073045dc0
WentTheFox <wentthefox@protonmail.com>
2026-04-10 01:30:35 +0200

    New translations timestamppicker.php (Portuguese)

--- lang/pt/timestampPicker.php
+++ lang/pt/timestampPicker.php
@@ -34,6 +34,11 @@ return [
   'table' => [
     'syntaxColumn' => 'Sintaxe de bate-papo',
     'resultColumn' => 'Resultado do exemplo',
+    'editFormats' => 'Customize formats',
+    'resetFormats' => 'Reset to defaults',
+    'hideFormat' => 'Hide this format',
+    'showFormat' => 'Show this format',
+    'unhideInProfile' => 'Unhide in profile settings',
   ],
   'usefulLinks' => [
     'lead' => 'Você também pode achar úteis esses:',
