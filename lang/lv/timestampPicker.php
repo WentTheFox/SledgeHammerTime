0f35fba35f9b6146181f2d4a7876f149f654d6e4
WentTheFox <wentthefox@protonmail.com>
2026-04-10 01:30:50 +0200

    New translations timestamppicker.php (Latvian)

--- lang/lv/timestampPicker.php
+++ lang/lv/timestampPicker.php
@@ -34,6 +34,11 @@ return [
   'table' => [
     'syntaxColumn' => 'Čata sintakses',
     'resultColumn' => 'Rezultāta piemērs',
+    'editFormats' => 'Customize formats',
+    'resetFormats' => 'Reset to defaults',
+    'hideFormat' => 'Hide this format',
+    'showFormat' => 'Show this format',
+    'unhideInProfile' => 'Unhide in profile settings',
   ],
   'usefulLinks' => [
     'lead' => 'Jūs varat arī atrast šos noderīgus:',
