cf430dbcda610b4a9f80f3c0edc898f5b5baab13
WentTheFox <wentthefox@protonmail.com>
2026-04-10 01:30:22 +0200

    New translations timestamppicker.php (Danish)

--- lang/da/timestampPicker.php
+++ lang/da/timestampPicker.php
@@ -34,6 +34,11 @@ return [
   'table' => [
     'syntaxColumn' => 'Syntaks',
     'resultColumn' => 'Resultat',
+    'editFormats' => 'Customize formats',
+    'resetFormats' => 'Reset to defaults',
+    'hideFormat' => 'Hide this format',
+    'showFormat' => 'Show this format',
+    'unhideInProfile' => 'Unhide in profile settings',
   ],
   'usefulLinks' => [
     'lead' => 'Du kan også finde disse nyttige:',
