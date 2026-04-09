61c6643d74ac937c7a424815345bf79e6a17082f
WentTheFox <wentthefox@protonmail.com>
2026-04-10 01:30:24 +0200

    New translations timestamppicker.php (Greek)

--- lang/el/timestampPicker.php
+++ lang/el/timestampPicker.php
@@ -34,6 +34,11 @@ return [
   'table' => [
     'syntaxColumn' => 'Σύνταξη συνομιλίας',
     'resultColumn' => 'Παράδειγμα αποτελέσματος',
+    'editFormats' => 'Customize formats',
+    'resetFormats' => 'Reset to defaults',
+    'hideFormat' => 'Hide this format',
+    'showFormat' => 'Show this format',
+    'unhideInProfile' => 'Unhide in profile settings',
   ],
   'usefulLinks' => [
     'lead' => 'Μπορεί επίσης να σας φανούν χρήσιμα:',
