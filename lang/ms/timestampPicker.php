1f52cf5c0609c865ea86ea458994e4abab49477a
WentTheFox <wentthefox@protonmail.com>
2026-04-10 01:30:52 +0200

    New translations timestamppicker.php (Malay)

--- lang/ms/timestampPicker.php
+++ lang/ms/timestampPicker.php
@@ -34,6 +34,11 @@ return [
   'table' => [
     'syntaxColumn' => 'Sintaks sembang',
     'resultColumn' => 'Contoh hasilnya',
+    'editFormats' => 'Customize formats',
+    'resetFormats' => 'Reset to defaults',
+    'hideFormat' => 'Hide this format',
+    'showFormat' => 'Show this format',
+    'unhideInProfile' => 'Unhide in profile settings',
   ],
   'usefulLinks' => [
     'lead' => 'Ini mungkin berguna untuk anda:',
