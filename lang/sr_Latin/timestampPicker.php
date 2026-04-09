2a592dea9d0413b9067209dd657122fa7e0810c9
WentTheFox <wentthefox@protonmail.com>
2026-04-10 01:30:56 +0200

    New translations timestamppicker.php (Serbian (Latin))

--- lang/sr_Latin/timestampPicker.php
+++ lang/sr_Latin/timestampPicker.php
@@ -34,6 +34,11 @@ return [
   'table' => [
     'syntaxColumn' => 'Sintaksa u poruci',
     'resultColumn' => 'Primer rezultata',
+    'editFormats' => 'Customize formats',
+    'resetFormats' => 'Reset to defaults',
+    'hideFormat' => 'Hide this format',
+    'showFormat' => 'Show this format',
+    'unhideInProfile' => 'Unhide in profile settings',
   ],
   'usefulLinks' => [
     'lead' => 'Ovo Vam takođe može biti korisno:',
