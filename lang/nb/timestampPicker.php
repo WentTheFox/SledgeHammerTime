a17e1369864a0fd431071623d3d59e72d459f753
WentTheFox <wentthefox@protonmail.com>
2026-04-10 01:30:55 +0200

    New translations timestamppicker.php (Norwegian Bokmal)

--- lang/nb/timestampPicker.php
+++ lang/nb/timestampPicker.php
@@ -34,6 +34,11 @@ return [
   'table' => [
     'syntaxColumn' => 'Chat syntaks',
     'resultColumn' => 'Eksempel resultat',
+    'editFormats' => 'Customize formats',
+    'resetFormats' => 'Reset to defaults',
+    'hideFormat' => 'Hide this format',
+    'showFormat' => 'Show this format',
+    'unhideInProfile' => 'Unhide in profile settings',
   ],
   'usefulLinks' => [
     'lead' => 'Du kan også finne dette nyttig:',
