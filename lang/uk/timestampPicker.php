abbbf3ea5621aba93b66bb7dbb3ede56769ce837
WentTheFox <wentthefox@protonmail.com>
2026-04-10 01:30:39 +0200

    New translations timestamppicker.php (Ukrainian)

--- lang/uk/timestampPicker.php
+++ lang/uk/timestampPicker.php
@@ -34,6 +34,11 @@ return [
   'table' => [
     'syntaxColumn' => 'Синтаксис чату',
     'resultColumn' => 'Приклад результату',
+    'editFormats' => 'Customize formats',
+    'resetFormats' => 'Reset to defaults',
+    'hideFormat' => 'Hide this format',
+    'showFormat' => 'Show this format',
+    'unhideInProfile' => 'Unhide in profile settings',
   ],
   'usefulLinks' => [
     'lead' => 'Вам також можуть бути корисними:',
