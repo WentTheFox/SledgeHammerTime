048ed67c246a430a3ed002760469625fa3674a7d
WentTheFox <wentthefox@protonmail.com>
2026-04-10 01:30:20 +0200

    New translations timestamppicker.php (Catalan)

--- lang/ca/timestampPicker.php
+++ lang/ca/timestampPicker.php
@@ -34,6 +34,11 @@ return [
   'table' => [
     'syntaxColumn' => 'Sintaxi al xat',
     'resultColumn' => 'Exemple',
+    'editFormats' => 'Customize formats',
+    'resetFormats' => 'Reset to defaults',
+    'hideFormat' => 'Hide this format',
+    'showFormat' => 'Show this format',
+    'unhideInProfile' => 'Unhide in profile settings',
   ],
   'usefulLinks' => [
     'lead' => 'També podeu trobar útil:',
