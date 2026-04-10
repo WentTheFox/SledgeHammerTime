4727b5e48bff8c9a1c818531f1c9c81edf906efc
WentTheFox <wentthefox@protonmail.com>
2026-04-10 22:45:59 +0200

    New translations timestamppicker.php (Hungarian)

--- lang/hu/timestampPicker.php
+++ lang/hu/timestampPicker.php
@@ -41,9 +41,8 @@ return [
     'unhideInProfile' => 'Megjelenítés a profilbeállításokban',
   ],
   'faq' => [
-    'title' => 'Frequently Asked Questions',
-    'description' => '
-        This section is English-only for now, and it\'s heavily based on content from <1>our Discord server</1>. Some links might not work as expected unless you are a member.',
+    'title' => 'Gyakran Ismételt Kérdések',
+    'description' => 'This section is English-only for now, and it\'s heavily based on content from <1>our Discord server</1>. Some links might not work as expected unless you are a member.',
   ],
   'usefulLinks' => [
     'lead' => 'Ezeket is hasznosnak találhatod:',
