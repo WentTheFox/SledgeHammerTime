3a5b5912b266827bc0c1a8b46928e324878a078d
WentTheFox <wentthefox@protonmail.com>
2026-04-18 21:24:00 +0200

    New translations timestamppicker.php (Turkish)

--- lang/tr/timestampPicker.php
+++ lang/tr/timestampPicker.php
@@ -63,7 +63,6 @@ return [
       'p' => 'ANSI renk kodlarını kullanarak renkli Discord mesajları oluşturan basit bir uygulama',
     ],
     "subreddit" => [
-      "header" => "r/SplitSecond",
       "p" => "Bu projenin oluşturulmasına ilham veren, yeterince takdir edilmemiş bir yarış oyunu için haftalık meydan okumalara ev sahipliği yapan topluluk",
     ],
     'competitors' => [
@@ -72,23 +71,20 @@ return [
         'p2' => 'You may want to check out these other Discord timestamp generators to find one that works best for you:',
       ],
       '3vfi' => [
-        'header' => 'r.3v.fi/discord-timestamps',
+        'header' => '',
         'p' => 'A simple and fast timestamp generator by 3ventic',
       ],
       'dabric' => [
-        'header' => 'timestamp.dabric.xyz',
+        'header' => '',
         'p' => 'Natural language Discord timestamp generator by dabric',
       ],
       'discordtimestampCom' => [
-        'header' => 'DiscordTimestamp.com',
         'p' => 'Free Discord timestamp generator with local timezone support by Sellframe Ltd.',
       ],
       'discordtimestampOrg' => [
-        'header' => 'DiscordTimestamp.org',
         'p' => 'Discord timestamp generator and time converter by DiscordTimestamp.org',
       ],
       'sesh' => [
-        'header' => 'sesh.fyi/timestamp',
         'p' => 'Create Discord markdown timestamps from within the Sesh scheduling bot ecosystem by Tunks',
       ],
     ],
