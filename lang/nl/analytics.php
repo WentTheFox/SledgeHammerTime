dab1dd2cc3790957fdd0357d4d72213bf373330d
WentTheFox <wentthefox@protonmail.com>
2026-06-14 10:40:40 +0200

    New translations analytics.php (Dutch)
    [ci skip]

--- lang/nl/analytics.php
+++ lang/nl/analytics.php
@@ -2,7 +2,7 @@
 
 return [
   'heading' => 'Paginaweergave statistieken',
-  'description' => 'Deze pagina bevat statistieken (zowel opgeslagen als weergegeven zonder persoonlijk identificeerbare gegevens) voor het totale aantal bekeken pagina\'s in de app gedurende de afgelopen 30 dagen.',
+  'description' => 'This page contains basic analytics (both stored and displayed without any personally identifiable details) for aggregate total page views in the app over the last :days days.',
   'collectionMethod' => 'De gegevens worden aan de serverkant verzameld, op basis van reacties die naar een beperkte set pagina\'s worden gestuurd. De bekeken pagina\'s worden individueel geregistreerd, maar dagelijks verzameld door een achtergrondproces.',
   'lastUpdated' => 'De informatie op deze pagina wordt voor een korte periode in de cache geplaatst om de belasting van de server te verminderen. De gegevens zijn voor het laatst bijgewerkt op <1/>.',
   'charts' => [
