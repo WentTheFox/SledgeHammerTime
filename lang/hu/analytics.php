e3e72e24a8989824912cfe75883fb2a8ed1bf734
WentTheFox <wentthefox@protonmail.com>
2026-06-14 10:40:33 +0200

    New translations analytics.php (Hungarian)
    [ci skip]

--- lang/hu/analytics.php
+++ lang/hu/analytics.php
@@ -2,7 +2,7 @@
 
 return [
   'heading' => 'Oldalmegtekintési Statisztikák',
-  'description' => 'Ez az oldal alapvető statisztikákat tartalmaz (személyazonosító részletek nélkül tárolva és megejelenítve) az alkalmazás összesített oldalmegtekintéseiről az elmúlt 30 napban.',
+  'description' => 'This page contains basic analytics (both stored and displayed without any personally identifiable details) for aggregate total page views in the app over the last :days days.',
   'collectionMethod' => 'Az adatokat a szerver gyűjti, egy korlátozott listán szereplő oldalakra küldött válaszok alapján. Az oldalmegtekintések egyenként tárolódnak, de egy háttérfolyamat napi szinten összegzi őket.',
   'lastUpdated' => 'Az oldal tartalma gyorsítótárból kerül betöltésre a szerver terhelésének csökkentése érdekében. A most látható adatok <1/> frissültek.',
   'charts' => [
