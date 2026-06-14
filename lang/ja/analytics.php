e5b3257302782ad1f44fc4c791997444e99b558d
WentTheFox <wentthefox@protonmail.com>
2026-06-14 10:40:36 +0200

    New translations analytics.php (Japanese)
    [ci skip]

--- lang/ja/analytics.php
+++ lang/ja/analytics.php
@@ -2,7 +2,7 @@
 
 return [
   'heading' => 'ページ閲覧の分析',
-  'description' => 'このページには、過去30日間のアプリの総閲覧数に関する基本的な分析（個人を特定できる詳細情報を含まないデータの保存と表示の両方）が含まれています。',
+  'description' => 'This page contains basic analytics (both stored and displayed without any personally identifiable details) for aggregate total page views in the app over the last :days days.',
   'collectionMethod' => 'The data is collected on the server side, based on responses sent to a limited set of pages. The page views are recorded individually but aggregated daily by a background process.',
   'lastUpdated' => 'The information on this page is cached for a short period to reduce server load. The data you see was last updated <1/>.',
   'charts' => [
