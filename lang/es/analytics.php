3d04e90b69e8b15a7ed2c2e0d80e0d36fbfff733
WentTheFox <wentthefox@protonmail.com>
2026-06-14 10:40:22 +0200

    New translations analytics.php (Spanish)
    [ci skip]

--- lang/es/analytics.php
+++ lang/es/analytics.php
@@ -2,7 +2,7 @@
 
 return [
   'heading' => 'Análisis de Vista de Página',
-  'description' => 'Esta página contiene analíticas básicas (almacenadas y mostradas sin ningún dato personal identificable) para el total agregado de vistas a la página agregadas en la aplicación durante los últimos 30 días.',
+  'description' => 'This page contains basic analytics (both stored and displayed without any personally identifiable details) for aggregate total page views in the app over the last :days days.',
   'collectionMethod' => 'Los datos se recopilan en el lado del servidor, basándose en las respuestas enviadas a un conjunto limitado de páginas. Las vistas de las páginas se registran individualmente, pero se agregan diariamente por un proceso en segundo plano.',
   'lastUpdated' => 'La información en esta página se almacena en caché durante un corto período de tiempo para reducir la carga del servidor. Los datos que ves fueron actualizados por última vez <1/>.',
   'charts' => [
