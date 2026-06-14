285b0181bac57f2b7c03bd97e376b9bd283cb729
WentTheFox <wentthefox@protonmail.com>
2026-06-14 10:41:00 +0200

    New translations analytics.php (Malay)
    [ci skip]

--- lang/ms/analytics.php
+++ lang/ms/analytics.php
@@ -2,7 +2,7 @@
 
 return [
   'heading' => 'Analitik Halaman Dilihat',
-  'description' => 'Halaman ini mengandungi analitik asas (yang disimpan dan dipaparkan tanpa sebarang perincian kenal pasti peribadi) untuk jumlah halaman dilihat keseluruhannya secara agregat di apl merentasi 30 hari yang lepas.',
+  'description' => 'This page contains basic analytics (both stored and displayed without any personally identifiable details) for aggregate total page views in the app over the last :days days.',
   'collectionMethod' => 'Data ini dikumpul di sisi pelayan, berdasarkan tindak balas yang dihantar ke set halaman yang terhad. Halaman dilihat direkod secara individu tetapi teragregat secara harian oleh proses latar belakang.',
   'lastUpdated' => 'Maklumat di halaman ini disimpan ke cache bagi tempoh yang singkat untuk mengurangkan muatan pelayan. Data yang anda lihat dikemaskinikan buat kali terakhir pada <1/>.',
   'charts' => [
