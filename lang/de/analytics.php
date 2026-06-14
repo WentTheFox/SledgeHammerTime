88f6b0b76a006ae1b778438704aec811a5ea24e2
WentTheFox <wentthefox@protonmail.com>
2026-06-14 10:40:29 +0200

    New translations analytics.php (German)
    [ci skip]

--- lang/de/analytics.php
+++ lang/de/analytics.php
@@ -2,7 +2,7 @@
 
 return [
   'heading' => 'Seitenaufruf-Analysen',
-  'description' => 'Diese Seite enthält grundlegende Analysen (sowohl gespeichert als auch angezeigt ohne persönlich identifizierbare Details) für die aggregierten gesamten Seitenaufrufe in der App in den letzten 30 Tagen.',
+  'description' => 'This page contains basic analytics (both stored and displayed without any personally identifiable details) for aggregate total page views in the app over the last :days days.',
   'collectionMethod' => 'Die Erhebung der Daten findet serverseitig statt, basierend auf Rückmeldungen, die an eine begrenzte Anzahl von Webseiten gesendet werden. Die Seitenbesuche werden einzeln registriert, aber täglich von einem Hintergrundprogramm aggregiert.',
   'lastUpdated' => 'Die Informationen auf dieser Seite werden für einen kurzen Zeitraum zwischengespeichert, um die Serverlast zu reduzieren. Die Daten, die du siehst, wurden zuletzt <1/> aktualisiert.',
   'charts' => [
