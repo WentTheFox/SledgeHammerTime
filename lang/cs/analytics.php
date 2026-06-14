591b925ae0870ce6717a3a0fb5f41bc6742b1c49
WentTheFox <wentthefox@protonmail.com>
2026-06-14 10:40:26 +0200

    New translations analytics.php (Czech)
    [ci skip]

--- lang/cs/analytics.php
+++ lang/cs/analytics.php
@@ -2,7 +2,7 @@
 
 return [
   'heading' => 'Analytika zobrazení stránek',
-  'description' => 'Tato stránka obsahuje základní analytika (uložené i zobrazené bez jakýchkoli údajů umožňující zjištění totožnosti) o celkovém počtu zobrazení stránek v aplikaci za posledních 30 dní.',
+  'description' => 'This page contains basic analytics (both stored and displayed without any personally identifiable details) for aggregate total page views in the app over the last :days days.',
   'collectionMethod' => 'Tyto údaje jsou shromažďovány na straně serveru na základě odpovědí odeslaných na omezenou sadu stránek. Zobrazení stránek jsou zaznamenávána jednotlivě, ale zpracovávány jsou denně pomocí procesu na pozadí.',
   'lastUpdated' => 'Informace na této stránce jsou ukládány pouze krátkodobě, aby se snížilo zatížení serveru. Údaje, které vidíte, byly naposledy aktualizovány <1/>.',
   'charts' => [
